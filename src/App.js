e942e896ac9f65d56d8a8394a937c33c658192e9
github username <um_2003_8@hotmail.com>
2021-04-02 12:23:19 -0400

    remove unwanted code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
-
 function App() {
   return (
-    <div>
+    <div className="w-60 h-60 m-auto rounded-lg bg-blue-500 shadow">
+      <p className="text-2xl tracking-wider font-bold">
+        I am learning Tailwind CSS
+      </p>
     </div>
   );
 }
