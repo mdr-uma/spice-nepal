d81cea7e00a366f57d8d69e20d5304a312d27aac
Uma <um_2003_8@hotmail.com>
2021-11-28 21:18:04 -0500

    remove unwanted text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ function App() {
   return (
     <div className="w-60 h-60 m-auto rounded-lg bg-blue-500 shadow">
       <p className="text-2xl tracking-wider font-bold">
-        I am learning Tailwind CSS
       </p>
     </div>
   );
